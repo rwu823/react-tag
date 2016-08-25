076c06cb2065c3e914e21322be69533c6da674d0
RoCky Wu <rwu@cepave.com>
2016-08-26 06:45:29 +0800

    remove unused files

--- src/compareObj.js
+++
@@ -1,9 +0,0 @@
-const size = (o)=> {
-  return Object.keys(o).length
-}
-
-module.exports = (a, b)=> {
-  const hasDiff = Object.keys(a).some(k=> a[k] !== b[k])
-
-  return size(a) === size(b) && !hasDiff
-}
